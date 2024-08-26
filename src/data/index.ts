820260ad05721bbe3880f1a5566a7c7749f74319
Daniel <daniel.40.pompa@gmail.com>
2024-08-26 17:14:59 +0200

    feat: update currency list with HKD and KRW
    Added Hong Kong Dollar (HKD) and South Korean Won (KRW) to the array of top 10 most traded currencies for crypto transactions.

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,9 @@
 import { Currency } from '../types';
 
-/** An array of supported currencies with their respective codes and names. */
+/**
+ * An array of the 10 most traded currencies in the world for crypto asset transactions, along with their respective codes and names.
+ * These currencies are highly influential in both global financial markets and the cryptocurrency space.
+ */
 export const currencies: Currency[] = [
   { code: 'CHF', name: 'Franco suizo' },
   { code: 'GBP', name: 'Libra esterlina' },
@@ -10,4 +13,6 @@ export const currencies: Currency[] = [
   { code: 'CAD', name: 'Dólar canadiense' },
   { code: 'CNY', name: 'Yuan chino' },
   { code: 'JPY', name: 'Yen japonés' },
+  { code: 'HKD', name: 'Dólar de Hong Kong' },
+  { code: 'KRW', name: 'Won surcoreano' },
 ];
